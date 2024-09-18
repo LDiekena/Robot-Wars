fea52678870d6eb9c34a8cace129da5a42148156
LADIEKEN <Lara.Diekena@btc-ag.com>
2024-09-18 13:03:49 +0200

    Name + simples Board

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,2 +1,7 @@
 public class Board {
+    public static void main(String[] args) {
+        char[][] board = new char[5][5];
+
+
+    }
 }
--- src/main/java/Registration.java
+++ src/main/java/Registration.java
@@ -6,5 +6,21 @@ public class Registration {
         System.out.println("Bitte gebe deinen Namen ein: ");
         String name = sc.nextLine();
 
+        System.out.println("Hallo " + name + "! Viel Spaß beim Spielen!");
+
+        char[][] board = new char[10][10];
+
+        for(int i = 0; i < 10; i++) {
+            for(int j = 0; j < 10; j++) {
+                System.out.print(" " + board[i][j]);
+                if(j < 10-1) System.out.print(" │");
+            }
+
+            if (i < 10-1) {
+                System.out.print("\n───");
+                for(int j = 0; j < 10-1; j++) System.out.print("┼───");
+                System.out.println();
+            }
+        }
     }
 }
