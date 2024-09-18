bc97126957c21036739556111632a7acf2c82842
LADIEKEN <Lara.Diekena@btc-ag.com>
2024-09-18 12:34:19 +0200

    First commit

---
+++ src/main/java/Registration.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Registration {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bitte gebe deinen Namen ein: ");
+        String name = sc.nextLine();
+
+    }
+}
