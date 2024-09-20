7b4aae2f22c2b0f7f44261fcec9cda5aa4094e9e
LADIEKEN <Lara.Diekena@btc-ag.com>
2024-09-20 09:28:22 +0200

    Kommentare

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,10 +2,12 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
+        //Eingabeaufforderung Name des Spielers
         Scanner sc = new Scanner(System.in);
         System.out.println("Bitte gebe deinen Namen ein: ");
         String name = sc.nextLine();
 
+        //Anzeige der Auswahl an wählbaren Avatars
         System.out.println("Hallo " + name + "! Bitte wähle deinen Roboter zwischen den beiden folgenden" +
                 " Möglichkeiten aus.");
 
@@ -24,7 +26,6 @@ public class main {
         System.out.println("    `---''---`");
         System.out.println();
 
-
         System.out.println( "Roboter Nr. 2" );
         System.out.println("      \\_/     ");
         System.out.println("     (* *)     ");
@@ -35,15 +36,18 @@ public class main {
         System.out.println("    _(___)_    ");
         System.out.println("   [-]   [-]   ");
 
+        //Avatarauswahl des Spielers
         System.out.println("Treffe nun deine Entscheidung in dem du 1 oder 2 eingibst.");
         int avatar = sc.nextInt();
 
+        //Abfrage ob eingegebene Auswahl korrekt bis diese korrekt ist
         while (avatar != 1 && avatar != 2) {
             System.out.println("Deine Eingabe war ungültig, bitte gebe entweder eine 1 für Roboter Nr. 1" +
                     " ein oder die 2 für Roboter Nr. 2!");
             avatar = sc.nextInt();
         }
 
+        //Auswahlbestätigung
         System.out.println("Du hast den Roboter Nr. " + avatar + " gewählt.");
 
     }
