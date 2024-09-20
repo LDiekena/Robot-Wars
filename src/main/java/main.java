e8fd6c6d39685b34787211a2d13eb045148b0678
LADIEKEN <Lara.Diekena@btc-ag.com>
2024-09-20 10:02:53 +0200

    Basic Spielfeld + Kommentare (Position Platzhalter)

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,23 @@
 public class main {
     public static void main(String[] args) {
+        //Spielfeld Attribute
+        int boardLenght = 15;
+        int boardHight = 10;
+        int position;
 
+        //Spielfeld
+        while (boardHight >= 1) {
+            int i = boardLenght;
+            while (i > 1) {
+                System.out.print(" [ ]");
+                i--;
+            }
+            boardHight--;
+            System.out.println(" [ ]");
+        }
+
+        //Position des Roboters bei Start
+        System.out.println("Dein Roboter befindet sich zu Beginn in dem Feld (z|s).");
     }
+
 }
