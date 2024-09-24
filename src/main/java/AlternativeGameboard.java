16903203b56f9f2dafd3b0ce6f230bedf24b61fc
LDiekena <Lara.Diekena@btc-ag.com>
2024-09-24 14:06:02 +0200

    Test mit randomized Barrieren auf dem Spielfeld

--- src/main/java/AlternativeGameboard.java
+++ src/main/java/AlternativeGameboard.java
@@ -10,12 +10,47 @@ public class AlternativeGameboard {
         int posZeile = r.nextInt(9);
         int posSpalte = r.nextInt(14);
 
+        //Avatar
+        int avatar = 2; //Für Testzwecke starr
+        char avatar1 = '§';
+        char avatar2 = '∆';
+        char gewaehlterAvatar = ' ';
+
+        if (avatar == 1) {
+            gewaehlterAvatar = avatar1;
+        } else if (avatar == 2) {
+            gewaehlterAvatar = avatar2;
+        } else {
+            System.out.println("Fehler beim zuweisen des Avatars.");
+        }
+
+        //Barrieren für das Spielfeld       TODO: Mehrere Barrierenfelder von den verschiedenen Arten
+        String steinBarriere = "▲";
+        String pflanzeBarriere = "♣";
+        String wasserBarriere = "≋";
+
+        int posZeileSteinBarrier = r.nextInt(9);
+        int posSpalteSteinBarrier = r.nextInt(14);
+
+        int posZeilePflanzeBarrier = r.nextInt(9);
+        int posSpaltePflanzeBarrier = r.nextInt(14);
+
+        int posZeileWasserBarrier = r.nextInt(9);
+        int posSpalteWasserBarrier = r.nextInt(14);
+
         //Spielfelderstellung
         for (int i = 0; i < spielfeld.length; i++) {
             for (int j = 0; j < spielfeld[i].length - 1; j++) {
-                if (i == posZeile && j == posSpalte) {
-                    //TODO: Einbau Symbole je nach Avatar
-                    System.out.print(" [X]");
+                if (i == posZeile && j == posSpalte && gewaehlterAvatar == avatar1) {
+                    System.out.print(" [" + avatar1 + "]");
+                } else if (i == posZeile && j == posSpalte && gewaehlterAvatar == avatar2) {
+                    System.out.print(" [" + avatar2+ "]");
+                } else if (i == posZeileSteinBarrier && j == posSpalteSteinBarrier) {
+                    System.out.print(" [" + steinBarriere+ "]");
+                } else if (i == posZeilePflanzeBarrier && j == posSpaltePflanzeBarrier) {
+                    System.out.print(" [" + pflanzeBarriere + "]");
+                } else if (i == posZeileWasserBarrier && j == posSpalteWasserBarrier) {
+                    System.out.print(" [" + wasserBarriere + "]");
                 } else {
                     System.out.print(" [ ]");
                 }
@@ -26,7 +61,6 @@ public class AlternativeGameboard {
         posZeile++;
 
         System.out.println("Dein Roboter befindet sich auf Position (S:" + posSpalte + "|Z:" +  posZeile + ").");
-
 
     }
 
