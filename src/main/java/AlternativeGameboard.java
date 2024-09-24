11aafb32a1ef8a8bb1f713262ff591a8e6a2e59a
LDiekena <Lara.Diekena@btc-ag.com>
2024-09-24 07:43:22 +0200

    Fix AlternativeGameboard Anzeige in Spalte 0

--- src/main/java/AlternativeGameboard.java
+++ src/main/java/AlternativeGameboard.java
@@ -2,21 +2,22 @@ import java.util.Random;
 
 public class AlternativeGameboard {
     public static void main(String[] args) {
+        //Array in der Größe 10x15
         String[][] spielfeld = new String[10][15];
         Random r = new Random();
 
-
+        //Randomizer für die Startposition
         int posZeile = r.nextInt(9);
         int posSpalte = r.nextInt(14);
 
+        //Spielfelderstellung
         for (int i = 0; i < spielfeld.length; i++) {
-            for (int j = 1; j < spielfeld[i].length; j++) {
+            for (int j = 0; j < spielfeld[i].length - 1; j++) {
                 if (i == posZeile && j == posSpalte) {
                     System.out.print(" [X]");
                 } else {
                     System.out.print(" [ ]");
                 }
-
             }
             System.out.println(" [ ]");
         }
