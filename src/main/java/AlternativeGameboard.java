e6fbb5963e89fff144a7d37d5df0e7b14f9da14d
LDiekena <Lara.Diekena@btc-ag.com>
2024-09-25 08:09:50 +0200

    Test mit mehreren randomized Barrieren auf dem Spielfeld, Abfragenproblem besteht noch

--- src/main/java/AlternativeGameboard.java
+++ src/main/java/AlternativeGameboard.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
 
 public class AlternativeGameboard {
@@ -24,20 +27,61 @@ public class AlternativeGameboard {
             System.out.println("Fehler beim zuweisen des Avatars.");
         }
 
-        //Barrieren für das Spielfeld       TODO: Mehrere Barrierenfelder von den verschiedenen Arten
+        //Barrieren für das Spielfeld
+        int zStein = 5;
+        int sStein = 5;
+        int zPflanzen = 10;
+        int sPflanzen = 10;
+        int zWasser = 7;
+        int sWasser = 7;
+
+        List<Integer> steinZeile = new ArrayList<>();
+        List<Integer> steinSpalte = new ArrayList<>();
+        List<Integer> pflanzenZeile = new ArrayList<>();
+        List<Integer> pflanzenSpalte = new ArrayList<>();
+        List<Integer> wasserZeile = new ArrayList<>();
+        List<Integer> wasserSpalte = new ArrayList<>();
+
         String steinBarriere = "▲";
         String pflanzeBarriere = "♣";
         String wasserBarriere = "≋";
 
-        int posZeileSteinBarrier = r.nextInt(9);
-        int posSpalteSteinBarrier = r.nextInt(14);
+        while (steinZeile.size() < zStein) {
+            int posSteinBarrier = r.nextInt(9);
+            steinZeile.add(posSteinBarrier);
+            zStein--;
+        }
+        while (steinSpalte.size() < sStein) {
+            int posSteinBarrier = r.nextInt(14);
+            steinSpalte.add(posSteinBarrier);
+            sStein--;
+        }
+        while (pflanzenZeile.size() < zPflanzen) {
+            int posPflanzenBarrier = r.nextInt(9);
+            pflanzenZeile.add(posPflanzenBarrier);
+            zPflanzen--;
+        }
+        while (pflanzenSpalte.size() < sPflanzen) {
+            int posPflanzenBarrier = r.nextInt(14);
+            pflanzenSpalte.add(posPflanzenBarrier);
+            sPflanzen--;
+        }
+        while (wasserZeile.size() < zWasser) {
+            int posWasserBarrier = r.nextInt(9);
+            wasserZeile.add(posWasserBarrier);
+            zWasser--;
+        }
+        while (wasserSpalte.size() < sWasser) {
+            int posWasserBarrier = r.nextInt(14);
+            wasserSpalte.add(posWasserBarrier);
+            sWasser--;
+        }
 
-        int posZeilePflanzeBarrier = r.nextInt(9);
-        int posSpaltePflanzeBarrier = r.nextInt(14);
+        int anzahlStein = 0;
+        int anzahlPflanzen = 0;
+        int anzahlWasser = 0;
 
-        int posZeileWasserBarrier = r.nextInt(9);
-        int posSpalteWasserBarrier = r.nextInt(14);
-
+        //TODO: Abfrage ob Feld schon Belegt ist (Barrieren oder Spieler), manchmal Problem
         //Spielfelderstellung
         for (int i = 0; i < spielfeld.length; i++) {
             for (int j = 0; j < spielfeld[i].length - 1; j++) {
@@ -45,12 +89,15 @@ public class AlternativeGameboard {
                     System.out.print(" [" + avatar1 + "]");
                 } else if (i == posZeile && j == posSpalte && gewaehlterAvatar == avatar2) {
                     System.out.print(" [" + avatar2+ "]");
-                } else if (i == posZeileSteinBarrier && j == posSpalteSteinBarrier) {
+                } else if (i == steinZeile.get(anzahlStein) && j == steinSpalte.get(anzahlStein)) {
                     System.out.print(" [" + steinBarriere+ "]");
-                } else if (i == posZeilePflanzeBarrier && j == posSpaltePflanzeBarrier) {
+                    anzahlStein++;
+                } else if (i == pflanzenZeile.get(anzahlPflanzen) && j == pflanzenSpalte.get(anzahlPflanzen)) {
                     System.out.print(" [" + pflanzeBarriere + "]");
-                } else if (i == posZeileWasserBarrier && j == posSpalteWasserBarrier) {
+                    anzahlPflanzen++;
+                } else if (i == wasserZeile.get(anzahlWasser) && j == wasserSpalte.get(anzahlWasser)) {
                     System.out.print(" [" + wasserBarriere + "]");
+                    anzahlWasser++;
                 } else {
                     System.out.print(" [ ]");
                 }
@@ -61,7 +108,6 @@ public class AlternativeGameboard {
         posZeile++;
 
         System.out.println("Dein Roboter befindet sich auf Position (S:" + posSpalte + "|Z:" +  posZeile + ").");
-
     }
 
 
